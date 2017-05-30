a9e067ccab5ffa11b018fb7926bb7c00e190eae6
15056158 <15056158@myrp.edu.sg>
2017-05-30 10:39:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoalarmmanager/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.demoalarmmanager;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSetAlarm;
+    AlarmManager am;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSetAlarm = (Button) this.findViewById(R.id.btnAlarm);
+
+        btnSetAlarm.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND,5);
+
+                //Create a new PendingIntent and add it to Alarm Manager
+                Intent intent = new Intent(MainActivity.this, AlarmReceiverActivity.class);
+                int reQCode = 12345;
+                PendingIntent pendingIntent = PendingIntent.getActivities(MainActivity.this, reQCode, new Intent[]{intent}, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //Get Alarm Manager instance
+                am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+
+                //set the alarm
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+
+            }
+        });
+
+    }
+}
